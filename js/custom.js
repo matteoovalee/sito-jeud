4115550c373a94f931833291fffea79bf0ae5e55
matteoovalee <matteovale05@proton.me>
2025-02-18 13:02:14 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Back to Top Arrow AND Hide Navbar
+  const backToTop = document.getElementById("backToTop");
+  const header = document.querySelector(".header_section");
+  let prevScrollPos = window.scrollY;
+
+  if (backToTop && header) { // Check if both elements exist
+    // Show/hide the arrow and navbar based on scroll position
+    window.onscroll = function () {
+      const currentScrollPos = window.scrollY;
+
+      // Back to Top Arrow
+      if (currentScrollPos > 300) {
+        backToTop.classList.add("show");
+      } else {
+        backToTop.classList.remove("show");
+      }
+
+      // Hide/Show Navbar
+      if (prevScrollPos > currentScrollPos) {
+        header.classList.remove("hide");
+      } else {
+        header.classList.add("hide");
+      }
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    // Scroll to top when the arrow is clicked
+    backToTop.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth", // Smooth scroll
+      });
+    });
+
+    //Slide In Animation
+    const slideInLeftElements = document.querySelectorAll(".slide-in-left");
+    const slideInRightElements = document.querySelectorAll(".slide-in-right");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    });
+
+    slideInLeftElements.forEach((element) => {
+      observer.observe(element);
+    });
+
+    slideInRightElements.forEach((element) => {
+      observer.observe(element);
+    });
+
+
+  }
+});
+
+
+/** google_map js 
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+*/
+
+
+// lightbox gallery
+$(document).on("click", '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
